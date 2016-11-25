8370dc00a0ce81593c0ec0872166df187cc1134d
babiprocs <gabrielihendges@gmail.com>
2016-11-24 22:33:12 -0200

    Configuração do email de recuperação de senha para html :tada:

--- src/seguranca_informacao/senhas/SolicitaEmail.java
+++ src/seguranca_informacao/senhas/SolicitaEmail.java
@@ -11,7 +11,7 @@ public class SolicitaEmail {
     public static void main (String args[]){
         try{
             RecuperarSenha email = new RecuperarSenha();
-            email.EnviarEmail("mh.matheussouza@gmail.com", "123456");
+            email.EnviarEmail("gabrielihendges@gmail.com", "123456");
             
         }catch(Exception ex){
             
