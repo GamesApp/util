d71ae8e0195633074858cd5ef7971c835ed78f39
babiprocs <gabrielihendges@gmail.com>
2016-11-28 22:50:09 -0200

    Ajustes na recuperação de senha e ajustes na criptografia das senhas :pray:

--- src/Criptografia/ExemploCriptografia.java
+++ src/Criptografia/ExemploCriptografia.java
@@ -6,15 +6,27 @@ import java.text.Normalizer;
 public class ExemploCriptografia {
     
     public static void main(String[] args) {
-        
-        CodCifraDeVigenere cvcifrar = new CodCifraDeVigenere("é_quarterão-2016");
-        System.out.println("cifrado: "+ cvcifrar.cifrar());
-        String msgdecifrar=cvcifrar.cifrar();
-        
-        CodCifraDeVigenere cvdecifrar = new CodCifraDeVigenere(msgdecifrar);
-        System.out.println("decifrado: "+ cvdecifrar.decifrar());
-        
+       
  }
+    
+    public String GeraCriptografia(String senha, int cod){
+        String retorno="";
+        
+        if (cod==1) {
+            //criptografar
+            CodCifraDeVigenere criptografar = new CodCifraDeVigenere(senha);
+            retorno = criptografar.cifrar();
+            System.out.println("senha criptografada: "+ retorno);
+            
+        } else if(cod==2){
+            //descriptografar
+            CodCifraDeVigenere descriptografar = new CodCifraDeVigenere(senha);
+            retorno = descriptografar.decifrar();
+            System.out.println("senha descriptografada: "+ retorno);
+        }
+        
+        return retorno;
+    }
 
     
     
